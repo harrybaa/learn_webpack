8167c02ce537c5013d50d320622d54a447ffb553
Harry Liu <harrybaa@gmail.com>
2017-07-20 22:11:38 +0800

    basics

---
+++ app/Greeter.js
@@ -0,0 +1,5 @@
+module.exports = function() {
+  var greatingDiv = document.createElement('div');
+  greatingDiv.textContent = 'Hello! This is my first demo for Webpack.';
+  return greatingDiv;
+};
